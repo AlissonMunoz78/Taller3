90eb8a761049f5aae0397d64cb0e4c66cda58594
AlissonMunoz78 <alisson.munoz@epn.edu.ec>
2025-05-04 23:10:38 -0500

    Add files via upload

---
+++ objetos-arreglos.js
@@ -0,0 +1,71 @@
+
+// OBJETO 
+
+const publicacion = {
+    cuenta: "lagambetasports",
+    descripcion: `¡SE NOS FUE OTRA JORNADA DE LOCURA! 🔥🇪🇸El Barça sigue mandando con 4 puntos de ventaja sobre el Madrid...
+    pero ¡la próxima semana será DECISIVA! 👀🏴La batalla por los boletos a Champions y Europa League está al rojo vivo 
+    en las últimas jornadas. ¡Nadie quiere quedarse fuera! 🏴⚽🇮🇹 El Napoli sigue líder, pero el Inter no afloja... 
+    ¡Solo 3 puntos de diferencia! 😱🇩🇪 El de siempre: ¡Bayern Múnich, campeón una vez más en la Bundesliga! 🇩🇪👑🇫🇷
+    En Francia, la lucha por Europa sigue candente... ¡Cada jornada los equipos lo dan todo!🔥#Fútbol #LaLiga 
+    #PremierLeague #SerieA #Bundesliga #Ligue1`,
+    meGustas: 10831,
+    comentarios: [
+      {
+        cuenta: "diaz.zadquiel",
+        texto: "Que pasa si el Puesto 5 y el puesto 4 empatan Los puntos?Quien va a Champions y quien va a EuropaLeague?",
+        meGustas: 1,
+        respuestas: [
+          {
+            cuenta: "cris_glez14",
+            texto: "@diaz.zadquiel Creo que se va el que se toma la diferencia de goles,aunque es muy difícil que pase eso",
+            meGustas: 4
+          }
+        ]
+      },
+      {
+        cuenta: "samueljpd",
+        texto: "El inter hace una semana peleaba por el triplete ahora se puede quedar sin nadaplete",
+        meGustas: 28,
+        respuestas: [
+          {
+            cuenta: "9tincho",
+            texto: `@samueljpd por eso no hay que hablar antes de tiempo y el Liverpool es un caso de eso,
+            pero que inter llega con la confianza necesaria para dar el batacazos! El PSG puede ir por el triplete`,
+            meGustas: 1
+          }
+        ]
+      }
+    ],
+    imagen: "https://www.instagram.com/lagambetasports/p/DJP9fTjvr07/?img_index=1",
+    fechaPublicacion: "04/05/2025"
+  };
+
+// ARREGLOS
+
+const publicaciones =[
+    {
+        cuenta:"zerospokernoticias",
+        descripcion:`Sin bajas hispanas en el Día 1 del €100K!
+        Yaman Nakdali, Juan Pardo y Adrián Mateos superan sin sobresaltos el Día 1 del Super High Roller más caro del EPT Montecarlo 2025.
+        ¿Quién de ellos llegará más lejos? @pokerstarsespana`,
+        meGustas:731,
+        comentarios:4,
+    },
+    {
+        cuenta:"itssuitsquotes",
+        descripcion:`THEY REUNITED
+        -Follow @itssuitsquotes for more!
+        -#suits #harveyspecter #meganmarkle #mikeross #rachelzane`,
+        meGustas:11462,
+        comentarios:18,
+    },
+    {
+        cuenta:"vrdope",
+        descripcion:`Iron Man and Dr Doom’s Theory!#thanos #marvel #drdoom #doomsday #marvelcinematicuniverse #theend #thor
+        #lokigodofmischief #hulk #ironman #blackpanther #antman #marveluniverse #marvelcomics #reels #instalike #marvelstudios 
+        #villain #villains #daredevil #punisher #chaos #omg #excited #wow #captainamerica #steverodgers #marveluniverselive #crazy`,
+        meGustas:65174,
+        comentarios:474,
+    },
+];
